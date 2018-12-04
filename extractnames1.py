8108db12a4b38b3ecd9ce28409ccc9611166f607
Gwilym Hibbert <gwilhibbert@gmail.com>
2018-12-04 11:53:38 +0000

    Inital moonboard problem version

---
+++ extractnames1.py
@@ -0,0 +1,66 @@
+#this is a python file for extracting information from html
+import os
+def open_file():
+    count=1
+    check=0
+    problem_list=[]
+    while check==0:
+        filename=str(count)+".html"
+        print("trying to open"+filename)
+        try:
+            filedata=open(filename,"r")
+            problem_list=find_data(filedata)
+            save_problems(problem_list)
+            os.remove(filename)
+        except:
+            check=1
+        count+=1
+
+def find_data(filedata):
+    line=filedata.readline()
+    problem_line=""
+    while line:
+        test=line.find('<h3><a href="https://www.moonboard.com/Problems/View')
+        if test==-1:
+            line=filedata.readline()
+        else:
+            problem_line=line
+            line=""
+            filedata.close()
+    if problem_line!="":
+        extract=extract_data(problem_line)
+        return(extract)
+    filedata.close()
+
+def extract_data(problem_line):
+    check=0
+    item_list=problem_line.split('"')
+    problem_list=[]
+    for i in item_list:
+        position=i.find('https://www.moonboard.com/Problems')
+        if position!=-1:
+            problem_list.append(i)
+    return(problem_list)
+
+def saved_problems():
+    save_file=open("problem_list.txt","r")
+    saved_line=save_file.readline()
+    saved_list=[]
+    while saved_line:
+        saved_list.append(saved_line)
+        saved_line=save_file.readline()
+    save_file.close()
+    return(saved_list)
+
+def save_problems(save_list):
+    saved_list=saved_problems()
+    save_file=open("problem_list.txt","a")
+    for item in save_list:
+        if item not in saved_list:
+            save_file.write(item+"\r\n")
+    save_file.close()
+
+def main():
+    open_file()
+
+main()
---
+++ scrape_problems.py
@@ -0,0 +1,117 @@
+import urllib.request
+import os
+
+def get_line(problem_file,data_points):
+    line=problem_file.readline()
+    if line=="":
+        return(line)
+    line_items=line.split(".,.")
+    if len(line_items)!=data_points or line_items[0]=="\n" or line[0]=="#":
+        line=get_line(problem_file,data_points)
+    return(line)
+
+def get_existing(problem_file,problem_list=[]):
+    line=get_line(problem_file,11)
+    while line:
+        problem_list.append(line.split(".,.")[0])
+        line=get_line(problem_file,11)
+    return(problem_list)
+
+def get_addresses():
+    address_file=open("problem_list.txt","r")
+    line=get_line(address_file,1)
+    address_list=[]
+    while line:
+        if len(line[:-1])!=47:
+            address_list.append(line[:-1])
+        line=get_line(address_file,1)
+    return(address_list)
+
+def scrape(url):#downloads webpage passed to it and saves to temp location
+    scrape_file=open("temp.txt","w")
+    req = urllib.request.Request(
+        url, 
+        data=None, 
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        }
+    )
+
+    f = urllib.request.urlopen(req)
+    scrape_file.write(f.read().decode('utf-8'))
+    scrape_file.close()
+
+def get_scrape_line():#find the line with all the useful data in
+    scrape_file=open("temp.txt","r")
+    scrape_line=scrape_file.readline()
+    check=-1
+    while scrape_line and check==-1:
+        scrape_line=scrape_file.readline()
+        check=scrape_line.find('"Method')
+    return(scrape_line)
+
+def extract_data(name,line):#takes the item to be found and extracts it
+    length=len(name)
+    position=line.find(name)+length+3
+    search_area=line[position:(position+30)]
+    search_list=search_area.split('"')
+    item=search_list[0]
+    return(item)
+
+def get_hold(line):#gets hold and sorts them in start midlle and end categories
+    hold_list=line.split("},{")
+    middle_hold=""
+    end_hold=""
+    start_hold=""
+    for item in hold_list:
+        data_list=item.split(",")
+        is_start=data_list[2].split(":")
+        if is_start[1]=="true":
+            start_hold+=(data_list[1].split(":"))[1][1:-1]+","
+        elif (data_list[3].split(":"))[1]=="true":
+            end_hold+=(data_list[1].split(":"))[1][1:-1]+","
+        else:
+            middle_hold+=(data_list[1].split(":"))[1][1:-1]+","
+    return(start_hold+".,."+middle_hold+".,."+end_hold)
+    
+
+def extract_holds(line):#removes everything from the string except holds info and calls get_hold
+    position=line.find("Moves")
+    line=line[(position+9):]
+    position=line.find("]")
+    line=line[0:(position-1)]
+    holds=get_hold(line)
+    return(holds)
+
+def get_items(line,address):#repeatedly calls extract_data for each item
+    factor_list=["Method","Name",'"Grade','"Id":1,"Description','Nickname','"Holdsetup":{"Id":15,"Description',"IsBenchmar"]
+    save_string=""
+    for item in factor_list:
+        extracted=extract_data(item,line)
+        save_string+=extracted+".,."
+    holds=extract_holds(line)
+    #print(holds)
+    save_string+=holds+"\r\n"
+    save_string=address+".,."+save_string
+    save_file=open("problems.txt","a")
+    save_file.write(save_string)
+    save_file.close()
+
+
+def main():
+    problem_file=open("problems.txt","r")
+    existing=get_existing(problem_file)
+    problem_file.close()
+    #print(existing)
+    addresses=get_addresses()
+    #print(addresses)
+    for address in addresses:
+        if address not in existing:
+            data=scrape(address)
+            line=get_scrape_line()
+            get_items(line,address)
+            os.remove("temp.txt")
+            
+main()
+
+
